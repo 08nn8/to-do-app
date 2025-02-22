79328f6036ae434bfc0747c1d2d3868719c232d3
NIGHT <laithhamarsheh68@gmail.com>
2025-02-22 12:42:13 -0800

    .

---
+++ src/components/Todo.jsx
@@ -0,0 +1,92 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import Todoitems from "./Todoitems";
+
+const Todo = () => {
+  const [todoList, setTodoList] = useState(localStorage.getItem("todos")?JSON.parse(localStorage.getItem("todos")) : []);
+
+  const inputRef = useRef();
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+
+    const NewTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+
+    setTodoList((prev) => [...prev, NewTodo]);
+    inputRef.current.value = "";
+  };
+
+  const deleteTodo = (id) => {
+    setTodoList((prvTodos) => {
+     return prvTodos.filter((todo) => todo.id !== id)
+    })
+  }
+
+  const toggle = (id) => {
+    setTodoList((prevTodos)=> {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {...todo, isComplete: !todo.isComplete}
+        }
+        return todo;
+      })
+    })
+  }
+
+  useEffect(() =>{
+    localStorage.setItem("todos", JSON.stringify(todoList))
+
+  }, [todoList])
+
+  return (
+    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
+      <div className="flex items-center mt-7 gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="icon icon-tabler icons-tabler-outline icon-tabler-layout-list text-black"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+          <path d="M4 14m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+        </svg>
+        <h2 className="text-3xl font-semibold text-black">To-do App</h2>
+      </div>
+      <div className="flex items-center my-7 bg-gray-200 rounded-full">
+        <input
+          ref={inputRef}
+          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          type="text"
+          placeholder="add your task"
+        />
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-blue-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
+          Add +
+        </button>
+      </div>
+      <div>
+      {todoList.map((item, index) => {
+        return <Todoitems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}  />
+      })}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
---
+++ src/components/Todoitems.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+const Todoitems = ({ text, id, isComplete, deleteTodo, toggle }) => {
+  return (
+    <div className="flex items-center my-3 gap-2">
+      <div
+        onClick={() => {
+          toggle(id);
+        }}
+        className="flex flex-1 items-center cursor-pointer"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="icon icon-tabler icons-tabler-outline icon-tabler-circle-check"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path
+            d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"
+            fill={isComplete ? "#4CAF50" : "none"}
+            stroke={isComplete ? "#4CAF50" : "currentColor"}
+          />
+
+          {isComplete && (
+            <path
+              d="M9 12l2 2l4 -4"
+              stroke="white"
+              strokeWidth="2"
+            />
+          )}
+        </svg>
+        <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${isComplete ? "line-through" : ""}`}>{text}</p>
+      </div>
+      <svg
+        onClick={() => {
+          deleteTodo(id);
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-trash cursor-pointer"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 7l16 0" />
+        <path d="M10 11l0 6" />
+        <path d="M14 11l0 6" />
+        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+      </svg>
+    </div>
+  );
+};
+
+export default Todoitems;
